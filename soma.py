5aa0c8699959d1d004f261c2f437febd6b3684e8
ISABEL DOS SANTOS PALMEIRA <isabel.palmeira@aluno.senac.rio>
2025-08-28 21:44:07 -0300

    Estruturas de repetição e range

---
+++ soma.py
@@ -0,0 +1,18 @@
+soma = 0
+#n = range (3)
+#print (n)
+for i in range (10):
+    n = int (input (f"Informe {i +1}ºum numero: "))
+    soma = soma + n
+    print(f"A soma é {soma}")
+
+
+
+
+n1 = int (input ("Informe um numero: "))
+n2 = int (input ("Informe outro numero: "))
+n3 = int (input ("Informe outro numero: "))
+
+soma = n1 + n2 + n3
+
+print (f"A soma é : {soma}")
